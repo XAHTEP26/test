dd4f91161dc926d01ce0e4db8bde2508e183cd2c
Grigoriy Kovalenko <g.kovalenko@pikabu.ru>
2023-09-06 15:13:05 +0300

    test

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+registerServiceWorker();
+
+async function registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register('./sw.js');
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (e) {
+            console.log(`Failed with error: ${e}`);
+        }
+    }
+}
---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e => {
+    console.log('installed');
+});
+
+self.addEventListener('activate', e => {
+    console.log('activated');
+});
+
+self.addEventListener('fetch', e => {
+    console.log('fetching');
+});

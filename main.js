c8f0e96da31ac761185bb3a34332e7eb7610c5c1
Grigoriy Kovalenko <g.kovalenko@pikabu.ru>
2023-09-06 15:17:44 +0300

    test

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-registerServiceWorker();
+main();
+
+async function main() {
+    await registerServiceWorker();
+    const response = await fetch('https://туристическаякарта26.рф/api/objects');
+    const data = await response.json();
+    console.log(data);
+}
 
 async function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         try {
             const registration = await navigator.serviceWorker.register('./sw.js');
             if (registration.installing) {
-                console.log("Service worker installing");
+                console.log('Service worker installing');
             } else if (registration.waiting) {
-                console.log("Service worker installed");
+                console.log('Service worker installed');
             } else if (registration.active) {
-                console.log("Service worker active");
+                console.log('Service worker active');
             }
         } catch (e) {
             console.log(`Failed with error: ${e}`);
